de6f1a0aa33b2690b7296eb1f66af050657b6f0c
Elielton Ramos <elieltonramos14@gmail.com>
2023-08-22 19:09:09 -0300

    Renderizando logo e imagem ilustrativa

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import styles from './login.module.css';
+import logoApp from '../../images/logoApp.svg';
+import tomatoImg from '../../images/tomato.svg';
 
 const initialState = {
   email: '',
@@ -26,33 +28,39 @@ function Login() {
   const btnActive = (regexEmail.test(form.email) && form.password.length > 6);
 
   return (
-    <form className={ styles.formLogin } onSubmit={ handleSubmitLogin }>
-      <h1 className={ styles.heading }>Login</h1>
-      <input
-        className={ styles.inputLogin }
-        name="email"
-        type="text"
-        data-testid="email-input"
-        value={ form.email }
-        onChange={ handleChange }
-      />
-      <input
-        className={ styles.inputLogin }
-        name="password"
-        type="password"
-        data-testid="password-input"
-        value={ form.password }
-        onChange={ handleChange }
-      />
-      <button
-        className={ styles.btnLogin }
-        type="submit"
-        data-testid="login-submit-btn"
-        disabled={ !btnActive }
-      >
-        Enter
-      </button>
-    </form>
+    <main>
+      <div className={ styles.backgroundLogin }>
+        <img src={ logoApp } alt="logo-app" className={ styles.logoApp } />
+        <img src={ tomatoImg } alt="tomate" className={ styles.tomato } />
+      </div>
+      <form className={ styles.formLogin } onSubmit={ handleSubmitLogin }>
+        <h1 className={ styles.heading }>Login</h1>
+        <input
+          className={ styles.inputLogin }
+          name="email"
+          type="text"
+          data-testid="email-input"
+          value={ form.email }
+          onChange={ handleChange }
+        />
+        <input
+          className={ styles.inputLogin }
+          name="password"
+          type="password"
+          data-testid="password-input"
+          value={ form.password }
+          onChange={ handleChange }
+        />
+        <button
+          className={ styles.btnLogin }
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ !btnActive }
+        >
+          Enter
+        </button>
+      </form>
+    </main>
   );
 }
 
