0d94dc568d4bc104f1740921cb682755b41f36c6
markoclimako <mkclimako@gmail.com>
2023-08-25 19:26:17 -0300

    corrigindo erros de lint

--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -4,12 +4,6 @@ import App from '../App';
 import renderWithRouter from './Mocks/Helpers';
 import { mockMealsFetch } from './helpers/mockFetch';
 
-// const idDrinkBottonBtn = 'drinks-bottom-btn';
-// const idMealBottonBtn = 'meals-bottom-btn';
-const otherUnknownTestId = 'Other / Unknown-category-filter';
-
-const beefTestId = 'Beef-category-filter';
-
 describe('testa o footer e suas funções na pagina /meals', () => {
   afterEach(() => {
     vi.clearAllMocks();
@@ -31,32 +25,7 @@ describe('testa o footer e suas funções na pagina /meals', () => {
 
     expect(window.location.pathname).toBe('/drinks');
 
-
-    // await user.click(mealsIcon);
-
-    // await screen.findByTestId(beefTestId);
+    await user.click(mealsIcon);
+    expect(window.location.pathname).toBe('/meals');
   });
 });
-
-// describe('testa o footer e suas funções na pagina /drinks', () => {
-//   afterEach(() => {
-//     vi.clearAllMocks();
-//   });
-
-//   beforeEach(async () => {
-//     global.fetch = vi.fn().mockImplementation(mockDrinksFetch as any);
-//   });
-//   // test('testa se os icones de bebidas e comidas estão na tela e redirecionam', async () => {
-//   //   const { user } = renderWithRouter(<App />, '/meals');
-//   //   const drinksIcon = screen.getByTestId('drinks-bottom-btn');
-//   //   const mealsIcon = screen.getByTestId('meals-bottom-btn');
-//   //   expect(drinksIcon).toBeInTheDocument();
-//   //   expect(mealsIcon).toBeInTheDocument();
-
-//   //   user.click(drinksIcon);
-//   //   await waitFor(() => expect(screen.getByTestId('page-title')).toHaveTextContent(/drinks/i));
-
-//   //   user.click(mealsIcon);
-//   //    await screen.findByTestId('page-title')
-//   // });
-// });
