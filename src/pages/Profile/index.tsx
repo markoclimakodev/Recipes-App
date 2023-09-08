32d9d1ec65ae2c3bc71580b556d2569081d73293
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-09-08 12:00:56 -0300

    adicionado funcionalidades da pagina de perfil

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,6 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+
 function Profile() {
+  const navigate = useNavigate();
+  const { email } = JSON.parse(localStorage.getItem('user') || '{}');
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
-    <h1>Profile</h1>
+    <section>
+      <p data-testid="profile-email">
+        { email }
+      </p>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
+
+    </section>
   );
 }
 
