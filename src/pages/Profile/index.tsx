2a67b23dd73a27bfc26841b371eb30c435857577
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-09-08 13:41:02 -0300

    css do profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import styles from './Profile.module.css';
+import doneIcon from '../../images/doneRecipesPageIcon.svg';
+import favoriteIcon from '../../images/favoritesPageIcon.svg';
+import logoutIcon from '../../images/Logout.svg';
 
 function Profile() {
   const navigate = useNavigate();
@@ -10,34 +14,49 @@ function Profile() {
   };
 
   return (
-    <section>
-      <p data-testid="profile-email">
-        { email }
-      </p>
+    <section className={ styles.profileContainer }>
+      <section className={ styles.emailContainer }>
+        <p data-testid="profile-email">
+          { email }
+        </p>
+      </section>
 
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => navigate('/done-recipes') }
-      >
-        Done Recipes
-      </button>
+      <section className={ styles.buttonsContainer }>
 
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => navigate('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
+        <div className={ styles.btnsDiv }>
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ () => navigate('/done-recipes') }
+          >
+            <img src={ doneIcon } alt="icon Done Recipes" />
+            Done Recipes
+          </button>
+        </div>
 
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ handleLogout }
-      >
-        Logout
-      </button>
+        <div className={ styles.btnsDiv }>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ () => navigate('/favorite-recipes') }
+          >
+            <img src={ favoriteIcon } alt="icon favorites" />
+            Favorite Recipes
+          </button>
+        </div>
+
+        <div className={ styles.btnsDiv }>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+          >
+            <img src={ logoutIcon } alt="icon logout" />
+            Logout
+          </button>
+        </div>
+
+      </section>
 
     </section>
   );
